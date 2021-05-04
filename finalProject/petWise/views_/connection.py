8e124dc09cc25b4d6851a69700bc5f2dae4b73c9
sirvesa <alon.schatz945@gmail.com>
2021-05-04 20:29:08 +0300

    convert connection to use the firebase singleton

--- finalProject/petWise/views_/connection.py
+++ finalProject/petWise/views_/connection.py
@@ -1,16 +1,11 @@
 from django.http import HttpResponse
-import firebase_admin
-from firebase_admin import firestore, credentials
-import os
 from singleton_decorator import singleton
-
-CRED = credentials.Certificate(os.path.join(os.path.dirname(__file__), "petwise-547d7-firebase-adminsdk-70638-65e70cc071.json"))
+from .firebase_connection import FirebaseConnection
 
 @singleton
 class Views:
     def __init__(self):
-        app = firebase_admin.initialize_app(CRED)
-        self.firestore_client = firestore.client(app)
+        self.firestore_client = FirebaseConnection().firestore_client
 
     def log_in(self, request):
         return HttpResponse("Hello, world. You're at the polls index.")
