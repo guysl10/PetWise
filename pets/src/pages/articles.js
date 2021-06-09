bcada56f7bccaca402aa78aa79eceaa9b14b4c3d
sirvesa <alon.schatz945@gmail.com>
2021-06-09 22:10:35 +0300

    added missing loading bar

--- pets/src/pages/articles.js
+++ pets/src/pages/articles.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import Article from "./article";
-import { Input, Space } from 'antd';
+import { Input, Space, Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+
+
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 const { Search } = Input;
+
 
 
 export default function Articles() {
@@ -10,7 +15,7 @@ export default function Articles() {
   const [fetchdArticles, setFetchedArticles] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('http://localhost:8000/petwise/summary/pets')
+    fetch('http://localhost:8000/petWise/articles')
       .then(response => response.json())
       .then(
         data => {
@@ -39,6 +44,15 @@ export default function Articles() {
     }
   };
 
+  const loading = () => {
+    if(fetchdArticles.length == 0) {
+      return <Spin tip="בטעינה..." indicator={antIcon} style={{
+        position: 'absolute', left: '50%', top: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}/>
+    }
+  };
+
   return (
     <>
       <div>
@@ -63,7 +77,11 @@ export default function Articles() {
         <section className="ftco-section bg-light">
           <div className="container">
             <div className="row d-flex">
-              {articles.map((data, key) => {
+              {
+                loading()
+              }
+              {fetchdArticles &&
+              fetchdArticles.length > 0 && articles.map((data, key) => {
                 return <Article key={key} datetime={data.datetime} desc={data.desc} title={data.title} date={data.date} link={data.link} />
               })}
 
